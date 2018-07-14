f1af6e27d519dbafc9ccbda3eca722b9132cc578
Blake Molina <blakeamolina@gmail.com>
2018-07-14 11:58:42 -0700

    Add payment form code

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+(function (window) {
+  "use strict";
+  var $ = window.jQuery;
+  var FORM_SELECTOR = "[data-coffee-order=\"form\"]";
+  var CHECKLIST_SELECTOR = "[data-coffee-order=\"checklist\"]";
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
+  var myTruck = new Truck("ncc-1701", new DataStore());
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  $("#payment").on("click",function(){
+    window.location.replace("payment.html");
+  });
+
+  formHandler.addSubmitHandler(function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
+
+  window.myTruck = myTruck;
+})(window);
